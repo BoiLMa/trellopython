00de5fdad3da9c71fd6a4e40b3e20eb91c3b2211
BoiLMa <30570@ma-web.nl>
2021-10-08 10:30:24 +0200

    slicewiseopdracht

---
+++ slicewise.py
@@ -0,0 +1,25 @@
+tekst = "De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde"
+print("Opdracht 1")
+print(tekst[37:41])
+print(tekst[43:48])
+print(tekst[61:66])
+print(" ")
+tekst2 = "SD vakken zijn bijvoorbeeld Python, UXD, Frontend development en Backend development. Wat leuk!"
+print("Opdracht 2")
+print(tekst2[28:34])
+print(tekst2[36:39])
+print(tekst2[41:61])
+print(tekst2[65:84])
+print(" ")
+tekst3 = "Wat is hier het laatste woord?"
+print("Opdracht 3")
+print(tekst3[24:30])
+print(" ")
+tekst4 = "Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee"
+print("Opdracht 4")
+print(tekst4[4:7])
+print(tekst4[28:32])
+
+
+
+
