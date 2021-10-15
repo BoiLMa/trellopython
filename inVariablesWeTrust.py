89e257b5f35b978aa266aa1239b25ccc4e55ccdb
BoiLMa <30570@ma-web.nl>
2021-10-15 09:58:13 +0200

    variables

---
+++ inVariablesWeTrust.py
@@ -0,0 +1,17 @@
+#"char"
+#float
+#"bool"
+#"int"
+#"string"
+
+print("Character1")
+print(float(10))
+test = 0
+print(test, "is" ,bool(test))
+test2 = 1
+print(test2, "is" ,bool(test2))
+print(int(50))
+print(int(80))
+a = "char"
+b = "acter"
+print(a + b)
