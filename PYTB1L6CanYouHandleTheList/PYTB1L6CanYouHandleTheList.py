e768b709e72d19683ee4d5fffc585070db9549de
BoiLMa <30570@ma-web.nl>
2021-11-20 16:56:23 +0100

    nieuwecommit

---
+++ PYTB1L6CanYouHandleTheList/PYTB1L6CanYouHandleTheList.py
@@ -0,0 +1,15 @@
+woorden = "hal"+"lo"
+List = ["Tekst", 8, 7, "A", woorden]
+
+countlist = List.count("Tekst")
+
+print(countlist)
+
+List.append("Nieuwe Tekst")
+
+print(List)
+
+List.clear()
+
+print(List)
+
--- PYTB1L6Groceries/PYTB1L6Groceries.py
+++ PYTB1L6Groceries/PYTB1L6Groceries.py
@@ -1,3 +1,11 @@
 GroceryList = ["Sausage", "Milk", "Cheese", "Bread"]
-ShoppingCart = ["",]
+ShoppingCart = ["Sausage", "Milk", "Cheese", "Bread"]
 
+for x in GroceryList:
+    if GroceryList == ShoppingCart:
+        print("Done Shopping")
+    else:
+        print("Continue Shopping")
+
+#ik denk dat je het best list kan gebruiken
+#dan hoef je maar heel weinig code te schrijven
